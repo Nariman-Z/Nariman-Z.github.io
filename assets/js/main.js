190f0ec9bf3f80a90efb469f0250213ccd3ca1dc
Nariman Ziaie <141274180+Nariman-Z@users.noreply.github.com>
2025-01-18 21:54:16 +0330

    personal website

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,6 +178,10 @@ if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+} else {
+  // Default to dark mode if no selection is found
+  document.body.classList.add(darkTheme)
+  themeButton.classList.add('uil-moon')
 }
 
 // Activate / deactivate the theme manually with the button
